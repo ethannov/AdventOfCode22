962932bc427f47677d46fee79511b679d21fbd86
Ethan <ethannovicio@tamu.edu>
2022-12-01 20:56:35 -0600

    Sum of calories of top three elves

---
+++ Day1/Max3Calories.py
@@ -0,0 +1,14 @@
+f = open("./Day1/input.txt")
+
+input = f.read()
+
+sumOfCals = []
+
+for cal in input.split("\n\n"):
+    sumOfCals.append(sum(int(x) for x in cal.split("\n")))
+    
+sumOfCals.sort(reverse=True)
+
+print("Sum of calories of top three elves is:", sumOfCals[0] + sumOfCals[1] + sumOfCals[2])
+    
+    
